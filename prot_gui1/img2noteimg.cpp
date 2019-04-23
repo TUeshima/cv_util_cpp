e83bed75ec781db15cf827b33565fde3c231c196
PTUeshima <taishi.ueshima@placeholder.co.jp>
2019-04-23 20:46:01 +0900

    nc

--- prot_gui1/img2noteimg.cpp
+++ prot_gui1/img2noteimg.cpp
@@ -2,7 +2,7 @@
 #include "img2noteimg.h"
 #include "cv_util.h"
 using namespace cv;
-
+using namespace std;
 cv::Mat img2noteimg::edge2noteimg(const cv::Mat& m, int w, int h)
 {
 	if (m.type() != CV_8UC1) throw std::exception("edge2noteimg needs CV_8UC1.");
@@ -30,6 +30,52 @@ cv::Mat img2noteimg::edge2noteimg(const cv::Mat& m, int w, int h)
 			}
 		}
 	}
-
 	return ret;
+}
+
+std::vector<pair<int, float>> img2noteimg::sum_scale_volume(const cv::Mat& m)
+{
+	if (m.type() != CV_8UC4) throw std::exception("sum_scale_volume needs CV_8UC4");
+	vector<pair<int, float>> idx_sum;
+	uchar* p = m.data;
+	float sum[
+	]
+	for (int y = 0; y < m.rows; ++y) {
+		float sum_x = 0;
+		for (int x = 0; x < m.cols; ++x) {
+			if (*(p + (y*m.cols + x) * 4 + 3) != 0) {
+				sum_x += 1;
+			}
+		}
+		idx_sum.push_back(make_pair((m.cols-y-1)%12, sum_x));
+	}
+	return idx_sum;
+}
+
+static int _sort_idx_sum(const pair<int, float>& a, const pair<int, float>& b)
+{
+	return (a.second - b.second) * 100000;
+}
+
+std::vector<int> img2noteimg::code_by_volume(const std::vector<int, float>& volume)
+{
+	vector<pair<int, float>> sorted = std::sort(volume.front, volume.end, _sort_idx_sum);
+	vector<int> code;
+	
+	if (sorted.size() <= 3) {
+		for (auto ite : sorted)
+		{
+			code.push_back(ite.first);
+		}
+		return code;
+	}
+
+
+
+
+}
+
+cv::Mat img2noteimg::decimate_by_code(const cv::Mat& m, const std::vector<int>& code, float rate_outsider)
+{
+
 }
--- prot_gui1/img2noteimg.h
+++ prot_gui1/img2noteimg.h
@@ -5,5 +5,8 @@ class img2noteimg
 {
 public:
 	static cv::Mat edge2noteimg(const cv::Mat& m, int w, int h);
+	static std::vector<pair<int, float>> sum_scale_volume(const cv::Mat& m);
+	static std::vector<int> code_by_volume(const std::vector<int, float>& volume);
+	static cv::Mat decimate_by_code(const cv::Mat& m, const std::vector<int>& code, float rate_outsider);
 };
 
